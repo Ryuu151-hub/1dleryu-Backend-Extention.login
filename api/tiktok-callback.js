aeab73e2e3d620eeab28bf9731914da720429fee
Ryuu151-hub <pox.john2025@gmail.com>
2026-08-02 10:19:00 +0800

    Update tiktok-callback.js

--- api/tiktok-callback.js
+++ api/tiktok-callback.js
@@ -18,8 +18,8 @@ export default async function handler(req, res) {
         'Cache-Control': 'no-cache',
       },
       body: new URLSearchParams({
-        client_key: process.env.sbaw1cszbxziwmm7b4,
-        client_secret: process.env.QQhlhL44rW0qRzZFfRrVn4E3tzN72jqg,
+        client_key: process.env.TIKTOK_CLIENT_KEY,
+        client_secret: process.env.TIKTOK_CLIENT_SECRET,
         code: code,
         grant_type: 'authorization_code',
         redirect_uri: process.env.TIKTOK_REDIRECT_URI,
